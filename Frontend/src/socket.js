51075b15323c6b3bd3afac53ccbd50db455d92dd
Shivansh Vishwakarma <147992386+technoshiva123@users.noreply.github.com>
2026-05-21 17:22:40 +0530

    Add timeout and transports options to socket connection

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -3,6 +3,8 @@ import { io } from 'socket.io-client';
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const socket = io(BACKEND_URL, {
+  transports: ["polling", "websocket"],
   autoConnect: false,
-  withCredentials: true
+  withCredentials: true,
+  timeout: 20000
 });
